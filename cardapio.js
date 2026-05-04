fb19425da799aa3b788a2c0c49fa7d83b01d4e0b
MUNIZ666__ <lucas.goncalvez.muniz@escola.pr.gov.br>
2026-05-04 11:47:48 +0000

    v1

--- cardapio.js
+++ cardapio.js
@@ -1,1 +1,13 @@
+const cardapio = [
+    {nome: "X-Burguer", preco: 18.90, categoria:"lanche"},
+    {nome: "batata-frita", preco: 9.50, categoria:"acompanhamento"},
+    {nome: "suco de laranja", preco: 7.00, categoria: "bebida"},
+    {nome: "milkshake", preco: 12.00, categoria: "sobremesa"}
+];
+console.log(cardapio); //impresão dos valores informados
+
+
+const pedido = [];
+
+console.log(cardapio[0].nome);
 
